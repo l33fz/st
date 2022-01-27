2903c3b86087b6c96c25b49ad1face40748dae3c
l33fz <itz0ver5000@gmail.com>
2022-01-27 21:07:01 +0800

    Gentoo version branched off Artix (initial branch commit)

--- config.h
+++ config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "VictorMono Nerd Font:pixelsize=17:antialias=true:autohint=true";
+static char *font = "Nimbus Mono:pixelsize=17:antialias=true:autohint=true";
 static int borderpx = 0;
 
 /*
@@ -94,52 +94,49 @@ char *termname = "st-256color";
 unsigned int tabspaces = 8;
 
 /* bg opacity */
-float alpha = 0.84;
+float alpha = 0.8;
 
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
-/* 8 normal colors */
-  [0] = "#232627", /* black   */
-  [1] = "#ed1515", /* red     */
-  [2] = "#11d116", /* green   */
-  [3] = "#f67400", /* yellow  */
-  [4] = "#1d99f3", /* blue    */
-  [5] = "#9b59b6", /* magenta */
-  [6] = "#1abc9c", /* cyan    */
-  [7] = "#fcfcfc", /* white   */
+	/* 8 normal colors */
+	"black",
+	"red3",
+	"green3",
+	"yellow3",
+	"blue2",
+	"magenta3",
+	"cyan3",
+	"gray90",
 
-  /* 8 bright colors */
-  [8]  = "#7f8c8d", /* black   */
-  [9]  = "#c0392b", /* red     */
-  [10] = "#1cdc9a", /* green   */
-  [11] = "#fdbc4b", /* yellow  */
-  [12] = "#3daee9", /* blue    */
-  [13] = "#8e44ad", /* magenta */
-  [14] = "#16a085", /* cyan    */
-  [15] = "#ffffff", /* white   */
+	/* 8 bright colors */
+	"gray50",
+	"red",
+	"green",
+	"yellow",
+	"#5c5cff",
+	"magenta",
+	"cyan",
+	"white",
 
-  /* special colors */
-  [256] = "#232627", /* background */
-  [257] = "#fcfcfc", /* foreground */
+	[255] = 0,
+
+	/* more colors can be added after 255 to use with DefaultXX */
+	"#cccccc",
+	"#555555",
+	"black",
+	"gray90", /* default foreground colour */
+	"black", /* default background colour */
 };
 
-/*
-* Default colors (colorname index)
-* foreground, background, cursor
-*/
-unsigned int defaultfg = 257;
-unsigned int defaultbg = 256;
-unsigned int defaultcs = 257;
-static unsigned int defaultrcs = 256;
 
 /*
-* Colors used, when the specific fg == defaultfg. So in reverse mode this
-* will reverse too. Another logic would only make the simple feature too
-* complex.
-*/
-static unsigned int defaultitalic = 7;
-static unsigned int defaultunderline = 7;
-
+ * Default colors (colorname index)
+ * foreground, background, cursor, reverse cursor
+ */
+unsigned int defaultfg = 7;
+unsigned int defaultbg = 258;
+unsigned int defaultcs = 256;
+static unsigned int defaultrcs = 257;
 
 /*
  * Default shape of cursor
